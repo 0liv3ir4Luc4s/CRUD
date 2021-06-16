7cdd20587bf68e33a0f9fae4e0b6b3fcc8c07e06
Lucas Oliveira <emailexterno4@gmail.com>
2021-06-16 16:07:37 -0300

    Adicionando método para executar requisições Ajax

--- js/Ajax.js
+++ js/Ajax.js
@@ -8,7 +8,7 @@ class Ajax {
 			} else if (window.ActiveXObject("Msxml2.XMLHTTP")) {
 				this.ajax = new window.ActiveXObject("Msxml2.XMLHTTP");
 			} else if (window.ActiveXObject("Microsoft.XMLHTTP")) {
-				this.ajax = new window.ActiveXObject("Microsoft.XMLHTTP"));
+				this.ajax = new window.ActiveXObject("Microsoft.XMLHTTP");
 			} else {
 				throw "Browser não compatível com Ajax";
 			}
@@ -20,5 +20,22 @@ class Ajax {
 	getAjax()
 	{
 		return this.ajax;
+	}
+
+	requisitar(arquivo, elemento) {
+		alertify.error("Erro no serv");
+		if (this.ajax) {
+			this.ajax.onreadystatechange = function() {
+				if (this.readyState == 4) {
+					if (this.status == 200 || this.status == 304) {
+						elemento.innerHTML = this.responseText;
+					} else {
+						alertify.error("Erro no servidor");
+					}
+				}
+			};
+			this.ajax.open("GET", arquivo);
+			this.ajax.send(null);
+		}
 	}
 }
