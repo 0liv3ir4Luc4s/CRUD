eeb549bef031657a96f713937fbc3c152b4a6d05
Lucas Oliveira <emailexterno4@gmail.com>
2021-06-18 06:26:57 -0300

    Adicionando action para consulta de curso

---
+++ consultarCurso.php
@@ -0,0 +1,32 @@
+<?php
+	require_once("controle/ControleCurso.php");
+	if (!empty($_POST["id"])) {
+		$controle = new ControleCurso();	
+		$curso = $controle->selecionarUm(intval($_POST["id"]));
+		if (!empty($curso)) {
+			echo "<table>";
+				echo "<thead>";
+					echo "<tr>";
+						echo "<th>Id</th>";
+						echo "<th>Nome</th>";
+						echo "<th>Coordenador</th>";
+					echo "</tr>";
+				echo "</thead>"; 
+				echo "<tbody>";
+					echo "<tr>";
+						echo "<td>{$curso->getId()}</td>";	
+						echo "<td>{$curso->getNome()}</td>";
+						echo "<td>{$curso->getCoordenador()}</td>";
+					echo "</tr>";
+				echo "</tbody>";
+			echo "</table>";
+		} else {
+			echo "<script>";
+			echo "alertify.error('Curso não cadastrado');";
+			echo "</script>";
+		}
+	} else {
+		echo "<script>";
+		echo "alertify.error('Não deixe campos em branco!');";
+		echo "</script>";
+	}	
