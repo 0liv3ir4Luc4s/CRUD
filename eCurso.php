6c2bf75f8e65aee0492cdca65bf2482b06f3613d
Lucas Oliveira <emailexterno4@gmail.com>
2021-06-18 00:06:25 -0300

    Adicionando action para edição de curso

---
+++ eCurso.php
@@ -0,0 +1,23 @@
+<?php
+	require_once("controle/ControleCurso.php");
+if (isset($_POST["id"]) && isset($_POST["nome"]) && isset($_POST["coordenador"])) {
+		$curso = new Curso();
+		$curso->setId(intval($_POST["id"]));
+		$curso->setNome($_POST["nome"]);
+		$curso->setCoordenador($_POST["coordenador"]);
+		$controle = new ControleCurso();	
+		if ($controle->editar($curso)) {
+			echo "<script>";
+			echo "alertify.success('Operação bem sucedida');";
+			echo "</script>";
+            header("Location: http://localhost/av_php/editarCurso.php");
+		} else {
+			echo "<script>";
+			echo "alertify.error('Erro na operação');";
+			echo "</script>";
+		}
+	} else {
+		echo "<script>";
+		echo "alertify.error('Não deixe campos em branco!');";
+		echo "</script>";
+	}	
