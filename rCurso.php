440e9bc84761c6ed043726a53820f456d073f3b1
Lucas Oliveira <emailexterno4@gmail.com>
2021-06-18 00:20:14 -0300

    Adicionando possiblidade de parametro GET na remoção de aluno

--- rCurso.php
+++ rCurso.php
@@ -1,12 +1,12 @@
 <?php
-	require_once("controle/ControleAluno.php");
-	if (!empty($_POST["matricula"]) || !empty($_GET["matricula"])) {
-		$controle = new ControleAluno();	
-		if ($controle->remover(intval($_POST["matricula"]))) {
+	require_once("controle/ControleCurso.php");
+	if (!empty($_POST["id"]) || !empty($_GET["id"])) {
+		$controle = new ControleCurso();	
+		if ($controle->remover(intval($_POST["id"]))) {
 			echo "<script>";
 			echo "alertify.success('Operação bem sucedida');";
 			echo "</script>";
-            !empty($_GET["matricula"]) ?: header("Location: http://localhost/av_php/removerAluno.php");
+			!empty($_GET["id"]) ?: header("Location: http://localhost/av_php/removerCurso.php");
 		} else {
 			echo "<script>";
 			echo "alertify.error('Erro na operação');";
