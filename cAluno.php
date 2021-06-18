f0c0e37dd5b2e51b19c119f45c90ae2b21bb28b2
Lucas Oliveira <emailexterno4@gmail.com>
2021-06-17 22:14:12 -0300

    Adicionando action para cadastro de aluno

---
+++ cAluno.php
@@ -0,0 +1,23 @@
+<?php
+	require_once("controle/ControleAluno.php");
+	if (isset($_POST["matricula"]) && isset($_POST["nome"]) && isset($_POST["email"])) {
+		$aluno = new Aluno();
+		$aluno->setMatricula(intval($_POST["matricula"]));
+		$aluno->setNome($_POST["nome"]);
+		$aluno->setEmail($_POST["email"]);
+		$controle = new ControleAluno();	
+		if ($controle->cadastrar($aluno)) {
+			echo "<script>";
+			echo "alertify.success('Operação bem sucedida');";
+			echo "</script>";
+            header("Location: http://localhost/av_php/cadastrarAluno.php");
+		} else {
+			echo "<script>";
+			echo "alertify.error('Erro na operação');";
+			echo "</script>";
+		}
+	} else {
+		echo "<script>";
+		echo "alertify.error('Não deixe campos em branco!');";
+		echo "</script>";
+	}	
