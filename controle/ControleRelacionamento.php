9709d35fa5bdf09706efb87cb3dce84b6f561d2a
Lucas Oliveira <emailexterno4@gmail.com>
2021-06-19 13:17:47 -0300

    Adicionando método para cadastro de relacionamento

---
+++ controle/ControleRelacionamento.php
@@ -0,0 +1,38 @@
+<?php
+	require_once("controle/Conexao.php");
+	require_once("modelo/Relacionamento.php");
+	require_once("modelo/Aluno.php");
+	require_once("modelo/Curso.php");
+	require_once("modelo/Turma.php");
+	
+	class ControleRelacionamento
+	{
+		public function cadastrar($relacionamento)
+		{
+			try {
+				$con = new Conexao("controle/configs.ini");	
+				$comando = $con->getPDO()->prepare("INSERT INTO relacionamento(aluno, turma, curso) VALUES (:a, :t, :c);");
+				$aluno = $relacionamento->getAluno()->getMatricula();
+				$comando->bindParam("a", $aluno);
+				$turma = $relacionamento->getTurma()->getId();
+				$comando->bindParam("t", $turma);
+				$curso = $relacionamento->getCurso()->getId();
+				$comando->bindParam("c", $curso);
+				$retorno = false;
+				if ($comando->execute())
+					$retorno = true;
+			} catch (PDOException $PDOex) {
+				$erro = "";
+			} catch (Exception $ex) {
+				$erro = "";
+			} finally {
+				echo "<script>";
+				echo "</script>";
+				$con->fecharConexao();
+				return $retorno;
+			}
+		}
+
+		
+	}
+
