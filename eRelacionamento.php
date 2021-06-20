05f984412d06ecc76501aae8afa8968550ba9fbd
Lucas Oliveira <emailexterno4@gmail.com>
2021-06-20 00:41:06 -0300

    Adicionando action para edição de relacionamento

---
+++ eRelacionamento.php
@@ -0,0 +1,24 @@
+<?php
+	require_once("controle/ControleRelacionamento.php");
+	if (!empty($_POST["id"]) && !empty($_POST["aluno"]) && !empty($_POST["turma"]) && !empty($_POST["curso"])) {
+		$rel = new Relacionamento();
+		$rel->setId(intval($_POST["id"]));
+		$rel->setAluno(intval($_POST["aluno"]));
+		$rel->setTurma(intval($_POST["turma"]));
+		$rel->setCurso(intval($_POST["email"]));
+		$controle = new ControleRelacionamento();	
+		if ($controle->cadastrar($rel)) {
+			echo "<script>";
+			echo "alertify.success('Operação bem sucedida');";
+			echo "</script>";
+            header("Location: http://localhost/av_php/editarRelacionamento.php");
+		} else {
+			echo "<script>";
+			echo "alertify.error('Erro na operação');";
+			echo "</script>";
+		}
+	} else {
+		echo "<script>";
+		echo "alertify.error('Não deixe campos em branco!');";
+		echo "</script>";
+	}	
