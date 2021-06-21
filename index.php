fd5034615670f702ae452e93620f839f08862009
Lucas Oliveira <emailexterno4@gmail.com>
2021-06-20 22:37:14 -0300

    Adicionando menu com opção relacionamento

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+		<title>Página Inicial</title>
+	</head>
+	<body>
+		<header>
+			<div class="navbar navbar-expand-sm navbar-dark bg-dark">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="index.php">Avaliação</a>
+					<button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-responsiva" aria-controls="navbar-responsiva" aria-expanded="false" aria-label="navbar-responsiva">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse flex-grow-0" id="navbar-responsiva">	
+						<nav>
+							<ul class="navbar-nav">
+								<li class="nav-item dropdown">
+									<a href="#" class="nav-link" role="button" id="dropRelacionamento" data-bs-toggle="dropdown" aria-expanded="false">Relacionamento</a>
+									<ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropRelacionamento">
+										<li><a href="index.php" class="dropdown-item">Lista</a></li>
+										<li><a href="cadastraRrelacionamento.php" class="dropdown-item">Cadastrar</a></li>
+										<li><a href="editarRelacionamento.php" class="dropdown-item">Editar</a></li>
+										<li><a href="removerRelacionamento.php" class="dropdown-item">Remover</a></li>
+									</ul>
+								</li>	
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</header>
+		<main>
+			
+		</main>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+	</body>
+</html>
